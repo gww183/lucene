1f55d51f3635223d889b2d6a6c43dce83f690621
guoweiwei <weiwei.guo@orange-car>
2019-05-25 19:49:13 +0800

    啊啊啊啊

---
+++ LUCENE_STU/src/java/com/orange/lucene/Asset.java
@@ -0,0 +1,11 @@
+package com.orange.lucene;
+
+public class Asset {
+
+	public static void main(String[] args) {
+		String s = null;
+		assert (s != null) ?  true : false;
+		System.out.println("end");
+	}
+
+}
